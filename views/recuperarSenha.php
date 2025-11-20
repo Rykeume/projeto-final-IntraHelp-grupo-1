130585e6c1da0cba31253f6e0da81e16c8f00abd
Ubiracy <ubiracy.cruz@estudante.ifto.edu.br>
2025-11-20 15:34:54 -0300

    Arquivos iniciais e banco de dados em SQLite

---
+++ views/recuperarSenha.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8" />
+  <title>Recuperar Senha</title>
+  <link rel="stylesheet" href="../style.css" />
+</head>
+<body>
+  <div class="container">
+    <h2>Recuperar Senha</h2>
+    <?php if (isset($_GET['sucesso']) && $_GET['sucesso'] == '1'): ?>
+      <div class="sucesso-msg">Senha alterada com sucesso!</div>
+    <?php endif; ?>
+    <?php if (isset($_GET['erro']) && $_GET['erro'] == '1'): ?>
+      <div class="erro-msg">E-mail ou senha incorretos.</div>
+    <?php endif; ?>
+    <form method="POST" action="../controllers/backend.php">
+      <input type="hidden" name="acao" value="recuperarSenha" />
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input id="email" type="email" name="email" required placeholder="E-mail"/>
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input id="senha" type="password" name="senha" required placeholder="Senha"/>
+      </div>
+      <button class="btn">Atualizar Senha</button>
+    </form>
+    <div class="link"><a href="paginaLogin.php" >Já tem uma conta? Faça login</a></div>
+    <div class="link"><a href="cadastro.php" >Não tem uma conta? Cadastre-se</a></div>
+  </div>
+</body>
+</html>
