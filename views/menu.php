a62b8c197c8975c4174ff92562fc5a8ae5ff7e2f
UbiracyCruz <164435487+UbiracyCruz@users.noreply.github.com>
2026-01-27 19:50:09 -0300

    Update menu.php

--- views/menu.php
+++ views/menu.php
@@ -15,7 +15,7 @@ if(eUsuarioLogado()){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/menu.css">
+    <link rel="stylesheet" href="./menu.css">
 </head>
 <body>
 
